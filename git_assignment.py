4f93488613e45404872038737d71ae496d8f4c2c
Afreen Shannu <afreenshannu03@gmail.com>
2026-02-25 22:49:05 +0530

    corrected the squaring logic

--- git_assignment.py
+++ git_assignment.py
@@ -1,5 +1,5 @@
 numbers = input("Enter numbers separated by space: ").split()
 count = len(numbers)
-square = count ** 3
+square = count ** 2
 print("number entered: ", count)
 print("square of the count: ", square)
